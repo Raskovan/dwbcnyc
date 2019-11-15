9a8e99ffa07e510a05b136b8390fdd530074babc
github username <github email address>
2019-11-14 22:47:01 -0500

    Major events added

--- src/Components/Program.jsx
+++ src/Components/Program.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Subtitle from './Subtitle'
 import Bio from './Bio'
-import { getDates, parseResponse } from '../helper.js'
+import { getDates, parseResponse, parseRegularResponse } from '../helper.js'
 import '../styles/Program.css'
 
 function Program(props) {
@@ -17,16 +17,16 @@ function Program(props) {
 		setBioIndex(index)
 	}
 
-	// const [majorEvents, setMajorEvents] = useState([])
+	const [majorEvents, setMajorEvents] = useState([])
 
 	useEffect(() => {
 		fetch(
-			'https://firewheel.herokuapp.com/widgets/119/full_events.json?callback=jQuery18203743304502847564_1570990678398&hostname=www.diamondway.org&version=1.0.1&_=1570990678529'
+			'https://firewheel.herokuapp.com/widgets/79/full_events.json?callback=jQuery18203743304502847564_1570990678398&hostname=www.diamondway.org&version=1.0.1&_=1570990678529'
 		)
 			.then(res => res.text())
 			.then(response => {
 				setNycEvents(parseResponse(response))
-				// setMajorEvents(majorEvents)
+				setMajorEvents(parseRegularResponse(response))
 			})
 			.catch(error => console.error('Error fetching:', error))
 		fetch('https://res.cloudinary.com/diamondway/image/list/tt.json')
@@ -46,6 +46,13 @@ function Program(props) {
 			image => image.context.custom.caption === name
 		)
 		return ttImage[0].public_id
+	}
+
+	const getLink = text => {
+		let regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi
+		let match = text.match(regex)
+		console.log(match)
+		return match[0]
 	}
 
 	return (
@@ -146,6 +153,36 @@ function Program(props) {
 							</div>
 						))}
 				</div>
+				{majorEvents.length > 0 && (
+					<div className='major_container'>
+						{majorEvents.map((event, index) => (
+							<div
+								className='program_major'
+								key={index}
+								style={{
+									textAlign: majorEvents.length < 2 ? 'center' : '',
+									flexGrow: majorEvents.length < 2 ? '1' : '0'
+								}}>
+								<p className='sub_sub_title'>
+									{event.location_summary.toUpperCase()} |{' '}
+									{getDates(event).toUpperCase()}
+								</p>
+								<p className='title_style'>{event.title}</p>
+								<div className='daily_button'>
+									<a
+										className='myButton'
+										style={{
+											width: majorEvents.length < 2 ? '50%' : ''
+										}}
+										href={getLink(event.description)}
+										target='_new'>
+										DETAILS
+									</a>
+								</div>
+							</div>
+						))}
+					</div>
+				)}
 			</div>
 			{nycEvents[bioIndex] && nycEvents[bioIndex].title && (
 				<Bio
