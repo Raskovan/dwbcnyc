1a1c651735d7322726e764c6acf3c299f3a22c2b
github username <github email address>
2019-11-23 21:55:19 -0500

    buddhism image adjustable width added

--- src/Components/Center.jsx
+++ src/Components/Center.jsx
@@ -20,50 +20,53 @@ export default function Center(props) {
 		<>
 			<Subtitle text={text.fields.title} />
 			<div
-				className='section_img center_img'
-				style={{ height: `${imageHeight}px` }}>
-				<img src={getLink()} width='100%' alt='NYC Center' />
+				className="section_img center_img"
+				style={{ height: `${imageHeight}px` }}
+			>
+				<img src={getLink()} width="100%" alt="NYC Center" />
 			</div>
 
-			<div className='container'>
-				<div className='container_text'>
+			<div className="container">
+				<div className="container_text">
 					<div>
-						<p className='body_text'>{text.fields.text}</p>
+						<p className="body_text">{text.fields.text}</p>
 					</div>
-					<div className='mp_container'>
-						<div className='mp_img_container'>
+					<div className="mp_container">
+						<div className="mp_img_container">
 							<img
-								width='100%'
+								width="100%"
 								src={require(`../assets/${
 									darkMode ? 'mp_logo_white.png' : 'mp_logo_black.png'
 								}`)}
-								alt='Manhattan Project Logo'
+								alt="Manhattan Project Logo"
 							/>
 						</div>
 						<div>
-							<p className='sub_sub_title'>{textMP.fields.title}</p>
+							<p className="sub_sub_title">{textMP.fields.title}</p>
 							<p
-								className='body_text'
-								dangerouslySetInnerHTML={{ __html: textMP.fields.text }}></p>
+								className="body_text"
+								dangerouslySetInnerHTML={{ __html: textMP.fields.text }}
+							></p>
 						</div>
 					</div>
 				</div>
-				<div className='container_img center_map'>
-					<div className='center_map_iframe'>
+				<div className="container_img center_map">
+					<div className="center_map_iframe">
 						<iframe
-							title='GMap'
-							width='100%'
-							height='100%'
-							frameBorder='0'
+							title="GMap"
+							width="100%"
+							height="100%"
+							frameBorder="0"
 							style={{ border: '0' }}
 							src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJHdOJgadZwokReTcfF04Wu4g&key=${process.env.REACT_APP_GOOGLE_MAPS}`}
-							allowFullScreen></iframe>
+							allowFullScreen
+						></iframe>
 					</div>
 					<div>
-						<p className='secondary_text'>
+						<p className="secondary_text">
 							Our Center Location:{'\n'}
 							114 East 28th Street, Suite #1 New York, NY 10016 {'\n'}
-							<a href='mailto:newyork@diamondway.org'>newyork@diamondway.org</a>
+							<a href="mailto:newyork@diamondway.org">newyork@diamondway.org</a>
 						</p>
 					</div>
 				</div>
