77be4f7867fe27c5603cb2e0eb5e117e2acca102
github username <github email address>
2020-01-05 17:45:21 -0500

    COlor added for secret word

--- src/Components/Quote.jsx
+++ src/Components/Quote.jsx
@@ -4,11 +4,15 @@ import '../styles/Quote.css'
 
 export default function Quote(props) {
 	const { text } = props
-	console.log(`
-  ╦╔═┌─┐┬─┐┌┬┐┌─┐┌─┐┌─┐┌─┐┬ ┬┌─┐┌┐┌┐┌┌─┐
-  ╠╩╗├─┤├┬┘│││├─┤├─┘├─┤│  ├─┤├┤ ││││││ │
-  ╩ ╩┴ ┴┴└─┴ ┴┴ ┴┴  ┴ ┴└─┘┴ ┴└─┘┘└┘└┘└─┘
-`)
+
+	const msg = `
+		╦╔═┌─┐┬─┐┌┬┐┌─┐┌─┐┌─┐┌─┐┬ ┬┌─┐┌┐┌┐┌┌─┐
+		╠╩╗├─┤├┬┘│││├─┤├─┘├─┤│  ├─┤├┤ ││││││ │
+		╩ ╩┴ ┴┴└─┴ ┴┴ ┴┴  ┴ ┴└─┘┴ ┴└─┘┘└┘└┘└─┘
+	`
+	const color = '#d22238'
+	console.log('%c' + msg, 'color:' + color)
+
 	return <div className="quote">{text.fields.text}</div>
 }
 
