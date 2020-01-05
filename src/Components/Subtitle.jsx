389ea3a970892003431898ec560b4990b2f297a2
github username <github email address>
2020-01-05 17:17:22 -0500

    further updates

--- src/Components/Subtitle.jsx
+++ src/Components/Subtitle.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import '../styles/Subtitle.css'
 
 function Subtitle(props) {
+	const { text } = props
 	return (
-		<div className='subtitle_container'>
+		<div className="subtitle_container">
 			<div>
-				<p className='subtitle_text'>{props.text}</p>
+				<p className="subtitle_text">{text}</p>
 			</div>
 			<div style={{ borderBottom: 'solid 1px #d22238', flexGrow: '1' }} />
 		</div>
 	)
+}
+
+Subtitle.propTypes = {
+	text: PropTypes.string
 }
 
 export default Subtitle
