a16b62580287f85a97e2d497212f2a44321f0713
github username <github email address>
2020-02-27 08:18:02 -0500

    Try

--- src/Components/Program.jsx
+++ src/Components/Program.jsx
@@ -59,17 +59,6 @@ function Program(props) {
 		return ttImage[0] && ttImage[0].public_id
 	}
 
-	// const getLink = text => {
-	// 	// eslint-disable-next-line
-	// 	const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
-	// 	const webRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi
-	// 	const emailMatch = text.match(emailRegex)
-	// 	const webMatch = text.match(webRegex)
-	// 	if (webMatch) return webMatch[0]
-	// 	else if (emailMatch) return `mailto:${emailMatch[0]}`
-	// 	else return
-	// }
-
 	const daily = programText.filter(text => text.fields.match === 'daily')[0]
 		.fields
 	const intro = programText.filter(text => text.fields.match === 'intro')[0]
